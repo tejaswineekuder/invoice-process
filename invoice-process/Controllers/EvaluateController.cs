f90bd9c098b5cab16029a5437f66f82e6c356ac4
Tejaswinee <tejaswinee@wildling.shoes>
2025-04-05 07:51:20 +0200

    - a POST /evaluate endpoint that accepts: o a document (PDF). o a JSON payload with invoice details - validates input format, required fields, and file size. - and logs

---
+++ invoice-process/Controllers/EvaluateController.cs
@@ -0,0 +1,37 @@
+﻿using invoice_process.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace invoice_process.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EvaluateController : ControllerBase
+    {
+        private readonly ILogger<EvaluateController> _logger;
+
+        public EvaluateController(ILogger<EvaluateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("evaluate")]
+        public async Task<IActionResult> Evaluate([FromForm] RequestDto request)
+        {
+            if (request.PdfFile == null || request.PdfFile.Length == 0)
+            {
+                _logger.LogError("No PDF file found, cannot be processed further");
+                return BadRequest("PDF file is required.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.JsonPayload))
+            {
+                _logger.LogError("Empty payload, cannot be processed further");
+                return BadRequest("JSON payload is required.");
+            }
+            _logger.LogInformation("Input verified , moving further");
+
+            return Ok("Input verified!");
+        }
+    }
+}
---
+++ invoice-process/DTOs/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace invoice_process.DTOs
+{
+    public class RequestDto
+    {
+        [Required]
+        public IFormFile? PdfFile { get; set; }
+
+        [Required]
+        public string? JsonPayload { get; set; }
+    }
+}
