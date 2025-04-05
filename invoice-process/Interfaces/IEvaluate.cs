e2d428d89fb85dc46dd53f1386855cd2316350b9
Tejaswinee <tejaswinee@wildling.shoes>
2025-04-05 09:37:34 +0200

    - summary dto - Evaluate interface and service to generate summary - returnsing evaluated summary in the result

---
+++ invoice-process/Interfaces/IEvaluate.cs
@@ -0,0 +1,15 @@
+﻿using invoice_process.DTOs;
+
+namespace invoice_process.Interfaces
+{
+    public interface IEvaluate
+    {
+        /// <summary>
+        ///    evaluates invoice and summarises.
+        /// </summary>
+        /// <returns>summary dto</returns>
+        /// <param name="">invoice dto with classification info</param>
+        Task<EvaluationSummaryDto> InvoiceEvaluation(InvoiceDto invoice);
+
+    }
+}
---
+++ invoice-process/Services/EvaluateService.cs
@@ -0,0 +1,20 @@
+﻿using invoice_process.DTOs;
+using invoice_process.Interfaces;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace invoice_process.Services
+{
+    public class EvaluateService : IEvaluate
+    {
+        public async Task<EvaluationSummaryDto> InvoiceEvaluation(InvoiceDto invoice)
+        {
+            //summarize the invoice, classification and rules into an object
+            return new EvaluationSummaryDto
+            {
+                EvaluationId = Guid.NewGuid().ToString(),
+                InvoiceId = invoice?.InvoiceId,
+                Classification = invoice?.Classification,
+            };
+        }
+    }
+}
